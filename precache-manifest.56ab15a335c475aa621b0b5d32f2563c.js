e5a5c0368febf75f7d02960af6fdf4d302b0d811
Ironiq Jr <69667698+ironiqjr@users.noreply.github.com>
2020-08-18 15:24:13 +0800

    Updates

---
+++ precache-manifest.56ab15a335c475aa621b0b5d32f2563c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811ba162bd894df21f07f41e4ca1b8a2",
+    "url": "/kikiya/index.html"
+  },
+  {
+    "revision": "48c54ebd8dc19a1887ea",
+    "url": "/kikiya/static/css/main.974343a9.chunk.css"
+  },
+  {
+    "revision": "c46507e99d0754c331dc",
+    "url": "/kikiya/static/js/2.13815831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kikiya/static/js/2.13815831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c54ebd8dc19a1887ea",
+    "url": "/kikiya/static/js/main.debae9f6.chunk.js"
+  },
+  {
+    "revision": "86a70b0152e07a536875",
+    "url": "/kikiya/static/js/runtime-main.1b9b0b4c.js"
+  },
+  {
+    "revision": "0791f02d27ccd8ac35d2fccfc06b63f5",
+    "url": "/kikiya/static/media/bubu.0791f02d.JPG"
+  }
+]);
